1343ab51d721ba013ae5f10261098e6d358bf18d
GS.Hyeon <qlqlqkqlqn@gmail.com>
2023-05-10 00:37:45 +0900

    first commit

---
+++ hangul.go
@@ -0,0 +1,503 @@
+package hangul_go
+
+import (
+	"bytes"
+)
+
+var (
+	choArr        = [19]rune{'ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ'}
+	jungArr       = [21]rune{'ㅏ', 'ㅐ', 'ㅑ', 'ㅒ', 'ㅓ', 'ㅔ', 'ㅕ', 'ㅖ', 'ㅗ', 'ㅘ', 'ㅙ', 'ㅚ', 'ㅛ', 'ㅜ', 'ㅝ', 'ㅞ', 'ㅟ', 'ㅠ', 'ㅡ', 'ㅢ', 'ㅣ'}
+	jongArr       = [28]rune{0, 'ㄱ', 'ㄲ', 'ㄳ', 'ㄴ', 'ㄵ', 'ㄶ', 'ㄷ', 'ㄹ', 'ㄺ', 'ㄻ', 'ㄼ', 'ㄽ', 'ㄾ', 'ㄿ', 'ㅀ', 'ㅁ', 'ㅂ', 'ㅄ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ'}
+	consonantsMap = map[rune]struct{}{
+		'ㄱ': {},
+		'ㄲ': {},
+		'ㄳ': {},
+		'ㄴ': {},
+		'ㄵ': {},
+		'ㄶ': {},
+		'ㄷ': {},
+		'ㄸ': {},
+		'ㄹ': {},
+		'ㄺ': {},
+		'ㄻ': {},
+		'ㄼ': {},
+		'ㄽ': {},
+		'ㄾ': {},
+		'ㄿ': {},
+		'ㅀ': {},
+		'ㅁ': {},
+		'ㅂ': {},
+		'ㅃ': {},
+		'ㅄ': {},
+		'ㅅ': {},
+		'ㅆ': {},
+		'ㅇ': {},
+		'ㅈ': {},
+		'ㅉ': {},
+		'ㅊ': {},
+		'ㅋ': {},
+		'ㅌ': {},
+		'ㅍ': {},
+		'ㅎ': {},
+	}
+	choMap = map[rune]int{
+		'ㄱ': 0,
+		'ㄲ': 1,
+		'ㄴ': 2,
+		'ㄷ': 3,
+		'ㄸ': 4,
+		'ㄹ': 5,
+		'ㅁ': 6,
+		'ㅂ': 7,
+		'ㅃ': 8,
+		'ㅅ': 9,
+		'ㅆ': 10,
+		'ㅇ': 11,
+		'ㅈ': 12,
+		'ㅉ': 13,
+		'ㅊ': 14,
+		'ㅋ': 15,
+		'ㅌ': 16,
+		'ㅍ': 17,
+		'ㅎ': 18,
+	}
+	jungMap = map[rune]int{
+		'ㅏ': 0,
+		'ㅐ': 1,
+		'ㅑ': 2,
+		'ㅒ': 3,
+		'ㅓ': 4,
+		'ㅔ': 5,
+		'ㅕ': 6,
+		'ㅖ': 7,
+		'ㅗ': 8,
+		'ㅘ': 9,
+		'ㅙ': 10,
+		'ㅚ': 11,
+		'ㅛ': 12,
+		'ㅜ': 13,
+		'ㅝ': 14,
+		'ㅞ': 15,
+		'ㅟ': 16,
+		'ㅠ': 17,
+		'ㅡ': 18,
+		'ㅢ': 19,
+		'ㅣ': 20,
+	}
+	jongMap = map[rune]int{
+		'ㄱ': 1,
+		'ㄲ': 2,
+		'ㄳ': 3,
+		'ㄴ': 4,
+		'ㄵ': 5,
+		'ㄶ': 6,
+		'ㄷ': 7,
+		'ㄹ': 8,
+		'ㄺ': 9,
+		'ㄻ': 10,
+		'ㄼ': 11,
+		'ㄽ': 12,
+		'ㄾ': 13,
+		'ㄿ': 14,
+		'ㅀ': 15,
+		'ㅁ': 16,
+		'ㅂ': 17,
+		'ㅄ': 18,
+		'ㅅ': 19,
+		'ㅆ': 20,
+		'ㅇ': 21,
+		'ㅈ': 22,
+		'ㅊ': 23,
+		'ㅋ': 24,
+		'ㅌ': 25,
+		'ㅍ': 26,
+		'ㅎ': 27,
+	}
+)
+
+func Disassemble(input string) []rune {
+	var result []rune
+	for _, r := range input {
+		if IsComplete(r) {
+			result = append(result, disassembleHangul(r)...)
+			continue
+		}
+		if !IsComplete(r) {
+			result = append(result, splitMixedJamo(r)...)
+			continue
+		}
+	}
+
+	return result
+}
+
+func DisassembleAsGroup(input string) [][]rune {
+	result := make([][]rune, 0, len(input))
+	for _, r := range input {
+		if IsComplete(r) {
+			result = append(result, disassembleHangul(r))
+			continue
+		}
+		split := splitMixedJamo(r)
+		result = append(result, split)
+	}
+	return result
+}
+
+func Assemble(input []rune) string {
+	var buf bytes.Buffer
+	for i := 0; i < len(input); {
+		r, step := buildHangul(input[i:])
+		buf.WriteRune(r)
+		i += step
+	}
+	return buf.String()
+}
+
+func IsComplete(r rune) bool {
+	return r >= '가' && r <= '힣'
+}
+
+func Search(a, b string) int {
+	ad := Disassemble(a)
+	bd := Disassemble(b)
+
+	return bytes.Index([]byte(string(ad)), []byte(string(bd)))
+}
+
+func IsCompleteAll(s string) bool {
+	for _, r := range s {
+		if !IsComplete(r) {
+			return false
+		}
+	}
+	return true
+}
+
+func IsConsonant(r rune) bool {
+	_, ok := consonantsMap[r]
+	return ok
+}
+
+func IsConsonantAll(s string) bool {
+	for _, r := range s {
+		if !IsConsonant(r) {
+			return false
+		}
+	}
+	return true
+}
+
+func IsVowel(r rune) bool {
+	_, ok := jungMap[r]
+	return ok
+}
+
+func IsVowelAll(s string) bool {
+	for _, r := range s {
+		if !IsVowel(r) {
+			return false
+		}
+	}
+	return true
+}
+
+func IsCho(r rune) bool {
+	_, ok := choMap[r]
+	return ok
+}
+
+func IsChoAll(s string) bool {
+	for _, r := range s {
+		if !IsCho(r) {
+			return false
+		}
+	}
+	return true
+}
+
+func IsJong(r rune) bool {
+	_, ok := jongMap[r]
+	return ok
+}
+
+func IsJongAll(s string) bool {
+	for _, r := range s {
+		if !IsJong(r) {
+			return false
+		}
+	}
+	return true
+}
+
+func EndsWithConsonant(s string) bool {
+	runes := []rune(s)
+	r := runes[len(runes)-1]
+	if IsComplete(r) {
+		disassembled := disassembleHangul(r)
+		return IsConsonant(disassembled[len(disassembled)-1])
+	}
+
+	return IsConsonant(r)
+}
+
+func EndsWith(input string, target rune) bool {
+	disassembled := Disassemble(input)
+	return disassembled[len(disassembled)-1] == target
+}
+
+func buildHangul(arr []rune) (rune, int) {
+	var cho, jung, jong rune
+
+	var generateHangul = func() rune {
+		hangul := 44032 + 28*(21*choMap[cho]) + 28*jungMap[jung] + jongMap[jong]
+		return rune(hangul)
+	}
+
+	if len(arr) == 1 {
+		return arr[0], 1
+	}
+
+	for step := 1; step <= len(arr); step++ {
+		if step == 1 {
+			if _, ok := choMap[arr[0]]; ok {
+				cho = arr[0]
+				continue
+			} else if _, ok := jungMap[arr[0]]; ok {
+				jung = arr[0]
+				continue
+			} else {
+				// 초성 없음
+				return arr[0], step
+			}
+		} else if step == 2 {
+			// 가능한 상황: [초성], [중성]
+			if _, ok := jungMap[arr[1]]; ok {
+				if cho == 0 {
+					// [중성] + 모음 상황
+					if isMixed, mixed := canMoumMixed(jung, arr[1]); isMixed {
+						return mixed, step
+					} else {
+						return jung, step - 1
+					}
+				} else {
+					// [초성] + 모음 상황
+					jung = arr[1]
+					continue
+				}
+			} else if _, ok := choMap[arr[1]]; ok {
+				// [초성] + 자음 상황
+				if isMixed, mixed := canJaumMixed(cho, arr[1]); isMixed {
+					return mixed, step
+				} else {
+					return cho, step - 1
+				}
+			} else {
+				return arr[0], step - 1
+			}
+		} else if step == 3 {
+			// 가능한 상황: [초성, 중성], [중성, 중성]
+			if _, ok := jungMap[arr[2]]; ok {
+				if cho == 0 {
+					// [중성, 중성] + 모음 상황 (종료)
+					return jung, step - 1
+				} else {
+					// [초성, 중성] + 모음 상황
+					if isMixed, mixed := canMoumMixed(jung, arr[2]); isMixed {
+						// [초성, 중성] + 모음 조합 가능
+						jung = mixed
+						continue
+					} else {
+						// [초성, 중성] + 모음 조합 불가능 (종료)
+						return generateHangul(), step - 1
+					}
+				}
+			} else if _, ok := jongMap[arr[2]]; ok {
+				// [초성, 중성] + 종성 상황
+				jong = arr[2]
+				continue
+			} else {
+				return generateHangul(), step - 1
+			}
+		} else if step == 4 {
+			// 가능한 상황: [초성, 중성, 종성], [초성, (중성, 중성)]
+			if _, ok := jongMap[arr[3]]; ok {
+				if jong == 0 {
+					// [초성, (중성, 중성)] + 종성 상황
+					jong = arr[3]
+					continue
+				} else {
+					// [초성, 중성, 종성] + 종성 상황
+					if isMixed, mixed := canJaumMixed(jong, arr[3]); isMixed {
+						// [초성, 중성, 종성] + 종성 조합 가능
+						jong = mixed
+						continue
+					} else {
+						// [초성, 중성, 종성] + 종성 조합 불가능 (종료)
+						return generateHangul(), step - 1
+					}
+				}
+			} else if _, ok := jungMap[arr[3]]; ok {
+				if jong == 0 {
+					// [초성, (중성, 중성)] + 모음 상황 (종료)
+					return generateHangul(), step - 1
+				} else {
+					// [초성, 중성, 종성] + 모음 상황
+					if _, ok := choMap[jong]; ok {
+						// 종성을 다음 글자 초성으로 붙일 수 있음
+						jong = 0
+						return generateHangul(), step - 2
+					} else {
+						// 종성을 다음 글자 초성으로 붙일 수 없음
+						return generateHangul(), step - 1
+					}
+				}
+			} else {
+				return generateHangul(), step - 1
+			}
+		} else if step == 5 {
+			// 가능한 상황: [초성, 중성, (종성, 종성)], [초성, (중성, 중성), 종성]
+			if _, ok := jongMap[arr[4]]; ok {
+				if isMixed, mixed := canJaumMixed(jong, arr[4]); isMixed {
+					// [초성, (중성, 중성), 종성] + 종성(조합 가능) 상황 (종료)
+					jong = mixed
+					continue
+				} else {
+					// [초성, 중성, (종성, 종성)] + 종성(조합 불가능) 상황 (종료)
+					// [초성, (중성, 중성), 종성] + 종성(조합 불가능) 상황 (종료)
+					return generateHangul(), step - 1
+				}
+			} else if _, ok := jungMap[arr[4]]; ok {
+				splitJong := splitMixedJamo(jong)
+				if len(splitJong) == 1 {
+					// [초성, (중성, 중성), 종성] + 모음 상황 (종료)
+					jong = 0
+					return generateHangul(), step - 2
+				} else {
+					// [초성, 중성, (종성, 종성)] + 모음 상황 (종료)
+					jong = splitJong[0]
+					return generateHangul(), step - 2
+				}
+			} else {
+				return generateHangul(), step - 1
+			}
+		} else if step == 6 {
+			// 가능한 상황 [초성, (중성, 중성), (종성, 종성)]
+			if _, ok := jungMap[arr[5]]; ok {
+				splitJong := splitMixedJamo(jong)
+				jong = splitJong[0]
+				return generateHangul(), step - 2
+			} else {
+				return generateHangul(), step - 1
+			}
+		}
+	}
+
+	return generateHangul(), len(arr)
+}
+
+func disassembleHangul(r rune) []rune {
+	var result []rune
+	temp := r - 44032
+	choTemp := temp / 588
+	jongIdx := temp % 28
+
+	result = append(result, splitMixedJamo(choArr[choTemp])...)
+	result = append(result, splitMixedJamo(jungArr[(temp%588)/28])...)
+	if jongArr[jongIdx] != 0 {
+		result = append(result, splitMixedJamo(jongArr[temp%28])...)
+	}
+
+	return result
+}
+
+func splitMixedJamo(r rune) []rune {
+	switch r {
+	case 'ㄳ':
+		return []rune{'ㄱ', 'ㅅ'}
+	case 'ㄵ':
+		return []rune{'ㄴ', 'ㅈ'}
+	case 'ㄶ':
+		return []rune{'ㄴ', 'ㅎ'}
+	case 'ㄺ':
+		return []rune{'ㄹ', 'ㄱ'}
+	case 'ㄻ':
+		return []rune{'ㄹ', 'ㅁ'}
+	case 'ㄼ':
+		return []rune{'ㄹ', 'ㅂ'}
+	case 'ㄽ':
+		return []rune{'ㄹ', 'ㅅ'}
+	case 'ㄾ':
+		return []rune{'ㄹ', 'ㅌ'}
+	case 'ㄿ':
+		return []rune{'ㄹ', 'ㅍ'}
+	case 'ㅀ':
+		return []rune{'ㄹ', 'ㅎ'}
+	case 'ㅄ':
+		return []rune{'ㅂ', 'ㅅ'}
+	case 'ㅘ':
+		return []rune{'ㅗ', 'ㅏ'}
+	case 'ㅙ':
+		return []rune{'ㅗ', 'ㅐ'}
+	case 'ㅚ':
+		return []rune{'ㅗ', 'ㅣ'}
+	case 'ㅝ':
+		return []rune{'ㅜ', 'ㅓ'}
+	case 'ㅞ':
+		return []rune{'ㅜ', 'ㅔ'}
+	case 'ㅟ':
+		return []rune{'ㅜ', 'ㅣ'}
+	case 'ㅢ':
+		return []rune{'ㅡ', 'ㅣ'}
+	default:
+		return []rune{r}
+	}
+}
+
+func canMoumMixed(r1, r2 rune) (bool, rune) {
+	if r1 == 'ㅗ' && r2 == 'ㅏ' {
+		return true, 'ㅘ'
+	} else if r1 == 'ㅜ' && r2 == 'ㅓ' {
+		return true, 'ㅝ'
+	} else if r1 == 'ㅗ' && r2 == 'ㅐ' {
+		return true, 'ㅙ'
+	} else if r1 == 'ㅜ' && r2 == 'ㅔ' {
+		return true, 'ㅞ'
+	} else if r1 == 'ㅡ' && r2 == 'ㅣ' {
+		return true, 'ㅢ'
+	} else if r1 == 'ㅗ' && r2 == 'ㅣ' {
+		return true, 'ㅚ'
+	} else if r1 == 'ㅜ' && r2 == 'ㅣ' {
+		return true, 'ㅟ'
+	} else {
+		return false, 0
+	}
+}
+
+func canJaumMixed(r1, r2 rune) (bool, rune) {
+	if r1 == 'ㄱ' && r2 == 'ㅅ' {
+		return true, 'ㄳ'
+	} else if r1 == 'ㄴ' && r2 == 'ㅈ' {
+		return true, 'ㄵ'
+	} else if r1 == 'ㄴ' && r2 == 'ㅎ' {
+		return true, 'ㄶ'
+	} else if r1 == 'ㄹ' && r2 == 'ㄱ' {
+		return true, 'ㄺ'
+	} else if r1 == 'ㄹ' && r2 == 'ㅁ' {
+		return true, 'ㄻ'
+	} else if r1 == 'ㄹ' && r2 == 'ㅂ' {
+		return true, 'ㄼ'
+	} else if r1 == 'ㄹ' && r2 == 'ㅅ' {
+		return true, 'ㄽ'
+	} else if r1 == 'ㄹ' && r2 == 'ㅌ' {
+		return true, 'ㄾ'
+	} else if r1 == 'ㄹ' && r2 == 'ㅍ' {
+		return true, 'ㄿ'
+	} else if r1 == 'ㄹ' && r2 == 'ㅎ' {
+		return true, 'ㅀ'
+	} else if r1 == 'ㅂ' && r2 == 'ㅅ' {
+		return true, 'ㅄ'
+	} else {
+		return false, 0
+	}
+}
---
+++ hangul_test.go
@@ -0,0 +1,764 @@
+package hangul_go
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestDisassemble(t *testing.T) {
+	tests := []struct {
+		name  string
+		input string
+		want  []rune
+	}{
+		{
+			name:  "'가나다'-기본동작",
+			input: "가나다",
+			want:  []rune{'ㄱ', 'ㅏ', 'ㄴ', 'ㅏ', 'ㄷ', 'ㅏ'},
+		},
+		{
+			name:  "'비행'-받침",
+			input: "비행",
+			want:  []rune{'ㅂ', 'ㅣ', 'ㅎ', 'ㅐ', 'ㅇ'},
+		},
+		{
+			name:  "'쓸다'-초성에 쌍자음",
+			input: "쓸다",
+			want:  []rune{'ㅆ', 'ㅡ', 'ㄹ', 'ㄷ', 'ㅏ'},
+		},
+		{
+			name:  "'의사'-중성에 복합모음",
+			input: "의사",
+			want:  []rune{'ㅇ', 'ㅡ', 'ㅣ', 'ㅅ', 'ㅏ'},
+		},
+		{
+			name:  "닭고기-종성에 복합 자음",
+			input: "닭고기",
+			want:  []rune{'ㄷ', 'ㅏ', 'ㄹ', 'ㄱ', 'ㄱ', 'ㅗ', 'ㄱ', 'ㅣ'},
+		},
+		{
+			name:  "옽ㅏ",
+			input: "옽ㅏ",
+			want:  []rune{'ㅇ', 'ㅗ', 'ㅌ', 'ㅏ'},
+		},
+		{
+			name:  "AB삵e$@%2324sdf낄캌ㅋㅋㅋㅋ",
+			input: "AB삵e$@%2324sdf낄캌ㅋㅋㅋㅋ",
+			want:  []rune{'A', 'B', 'ㅅ', 'ㅏ', 'ㄹ', 'ㄱ', 'e', '$', '@', '%', '2', '3', '2', '4', 's', 'd', 'f', 'ㄲ', 'ㅣ', 'ㄹ', 'ㅋ', 'ㅏ', 'ㅋ', 'ㅋ', 'ㅋ', 'ㅋ', 'ㅋ'},
+		},
+		{
+			name:  "뷁궬릪쯻튋",
+			input: "뷁궬릪쯻튋",
+			want:  []rune{'ㅂ', 'ㅜ', 'ㅔ', 'ㄹ', 'ㄱ', 'ㄱ', 'ㅜ', 'ㅔ', 'ㄹ', 'ㄹ', 'ㅡ', 'ㅣ', 'ㅍ', 'ㅉ', 'ㅡ', 'ㅣ', 'ㄹ', 'ㅂ', 'ㅌ', 'ㅜ', 'ㅣ', 'ㄹ', 'ㅂ'},
+		},
+		{
+			name:  "ㄳ",
+			input: "ㄳ",
+			want:  []rune{'ㄱ', 'ㅅ'},
+		},
+		{
+			name:  "ㅙ",
+			input: "ㅙ",
+			want:  []rune{'ㅗ', 'ㅐ'},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := Disassemble(tt.input)
+			if !assert.Equal(t, tt.want, result) {
+				t.Errorf("Disassemble() = %v, want %v", result, tt.want)
+			}
+		})
+	}
+}
+
+func TestDisassembleAsGroup(t *testing.T) {
+	tests := []struct {
+		name  string
+		input string
+		want  [][]rune
+	}{
+		{
+			name:  "기본동작-기본동작",
+			input: "가나다",
+			want:  [][]rune{{'ㄱ', 'ㅏ'}, {'ㄴ', 'ㅏ'}, {'ㄷ', 'ㅏ'}},
+		},
+		{
+			name:  "'비행'-받침",
+			input: "비행",
+			want:  [][]rune{{'ㅂ', 'ㅣ'}, {'ㅎ', 'ㅐ', 'ㅇ'}},
+		},
+		{
+			name:  "'쓸다'-초성에 쌍자음",
+			input: "쓸다",
+			want:  [][]rune{{'ㅆ', 'ㅡ', 'ㄹ'}, {'ㄷ', 'ㅏ'}},
+		},
+		{
+			name:  "'의사'-중성에 복합모음",
+			input: "의사",
+			want:  [][]rune{{'ㅇ', 'ㅡ', 'ㅣ'}, {'ㅅ', 'ㅏ'}},
+		}, {
+			name:  "'짧은'-종성에 복합자음",
+			input: "짧은",
+			want:  [][]rune{{'ㅉ', 'ㅏ', 'ㄹ', 'ㅂ'}, {'ㅇ', 'ㅡ', 'ㄴ'}},
+		},
+		{
+			name:  "닭고기-종성에 복합 자음",
+			input: "닭고기",
+			want:  [][]rune{{'ㄷ', 'ㅏ', 'ㄹ', 'ㄱ'}, {'ㄱ', 'ㅗ'}, {'ㄱ', 'ㅣ'}},
+		},
+		{
+			name:  "옽ㅏ",
+			input: "옽ㅏ",
+			want:  [][]rune{{'ㅇ', 'ㅗ', 'ㅌ'}, {'ㅏ'}},
+		},
+		{
+			name:  "AB삵e$@%2s낄캌ㅋ",
+			input: "AB삵e$@%2s낄캌ㅋ",
+			want:  [][]rune{{'A'}, {'B'}, {'ㅅ', 'ㅏ', 'ㄹ', 'ㄱ'}, {'e'}, {'$'}, {'@'}, {'%'}, {'2'}, {'s'}, {'ㄲ', 'ㅣ', 'ㄹ'}, {'ㅋ', 'ㅏ', 'ㅋ'}, {'ㅋ'}},
+		},
+		{
+			name:  "뷁궬릪쯻튋",
+			input: "뷁궬릪쯻튋",
+			want:  [][]rune{{'ㅂ', 'ㅜ', 'ㅔ', 'ㄹ', 'ㄱ'}, {'ㄱ', 'ㅜ', 'ㅔ', 'ㄹ'}, {'ㄹ', 'ㅡ', 'ㅣ', 'ㅍ'}, {'ㅉ', 'ㅡ', 'ㅣ', 'ㄹ', 'ㅂ'}, {'ㅌ', 'ㅜ', 'ㅣ', 'ㄹ', 'ㅂ'}},
+		},
+		{
+			name:  "ㄳ",
+			input: "ㄳ",
+			want:  [][]rune{{'ㄱ', 'ㅅ'}},
+		},
+		{
+			name:  "ㅙ",
+			input: "ㅙ",
+			want:  [][]rune{{'ㅗ', 'ㅐ'}},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if !assert.Equal(t, tt.want, DisassembleAsGroup(tt.input)) {
+				t.Errorf("TestDisassembleAsGroup() = %v, want %v", DisassembleAsGroup(tt.input), tt.want)
+			}
+		})
+	}
+}
+
+func TestAssemble(t *testing.T) {
+	tests := []struct {
+		name  string
+		input []rune
+		want  string
+	}{
+		{
+			name:  "기본동작",
+			input: []rune{'ㄱ', 'ㅏ', 'ㄴ', 'ㅏ', 'ㄷ', 'ㅏ'},
+			want:  "가나다",
+		},
+		{
+			name:  "비행",
+			input: []rune{'ㅂ', 'ㅣ', 'ㅎ', 'ㅐ', 'ㅇ'},
+			want:  "비행",
+		},
+		{
+			name:  "쓸다",
+			input: []rune{'ㅆ', 'ㅡ', 'ㄹ', 'ㄷ', 'ㅏ'},
+			want:  "쓸다",
+		},
+		{
+			name:  "의사",
+			input: []rune{'ㅇ', 'ㅡ', 'ㅣ', 'ㅅ', 'ㅏ'},
+			want:  "의사",
+		},
+		{
+			name:  "짧은",
+			input: []rune{'ㅉ', 'ㅏ', 'ㄹ', 'ㅂ', 'ㅇ', 'ㅡ', 'ㄴ'},
+			want:  "짧은",
+		},
+		{
+			name:  "닭고기",
+			input: []rune{'ㄷ', 'ㅏ', 'ㄹ', 'ㄱ', 'ㄱ', 'ㅗ', 'ㄱ', 'ㅣ'},
+			want:  "닭고기",
+		},
+		{
+			name:  "오타",
+			input: []rune{'ㅇ', 'ㅗ', 'ㅌ', 'ㅏ'},
+			want:  "오타",
+		},
+		{
+			name:  "AB삵e$@%2324sdf낄캌ㅋㅋㅋㅋ",
+			input: []rune{'A', 'B', 'ㅅ', 'ㅏ', 'ㄹ', 'ㄱ', 'e', '$', '@', '%', '2', '3', '2', '4', 's', 'd', 'f', 'ㄲ', 'ㅣ', 'ㄹ', 'ㅋ', 'ㅏ', 'ㅋ', 'ㅋ', 'ㅋ', 'ㅋ', 'ㅋ'},
+			want:  "AB삵e$@%2324sdf낄캌ㅋㅋㅋㅋ",
+		},
+		{
+			name:  "뷁궬릪쯻튋",
+			input: []rune{'ㅂ', 'ㅜ', 'ㅔ', 'ㄹ', 'ㄱ', 'ㄱ', 'ㅜ', 'ㅔ', 'ㄹ', 'ㄹ', 'ㅡ', 'ㅣ', 'ㅍ', 'ㅉ', 'ㅡ', 'ㅣ', 'ㄹ', 'ㅂ', 'ㅌ', 'ㅜ', 'ㅣ', 'ㄹ', 'ㅂ'},
+			want:  "뷁궬릪쯻튋",
+		},
+		{
+			name:  "ㄳ",
+			input: []rune{'ㄱ', 'ㅅ'},
+			want:  "ㄳ",
+		},
+		{
+			name:  "ㅙ",
+			input: []rune{'ㅗ', 'ㅐ'},
+			want:  "ㅙ",
+		},
+		{
+			name:  "ㅈ사",
+			input: []rune{'ㅈ', 'ㅅ', 'ㅏ'},
+			want:  "ㅈ사",
+		},
+		{
+			name:  "ㄳㄳ",
+			input: []rune{'ㄱ', 'ㅅ', 'ㄱ', 'ㅅ'},
+			want:  "ㄳㄳ",
+		},
+		{
+			name:  "ㅙㅙ",
+			input: []rune{'ㅗ', 'ㅐ', 'ㅗ', 'ㅐ'},
+			want:  "ㅙㅙ",
+		},
+		{
+			name:  "조ㅙ",
+			input: []rune{'ㅈ', 'ㅗ', 'ㅗ', 'ㅐ'},
+			want:  "조ㅙ",
+		},
+		{
+			name:  "ㅣㅙ",
+			input: []rune{'ㅣ', 'ㅗ', 'ㅐ'},
+			want:  "ㅣㅙ",
+		},
+		{
+			name:  "ㅃ짜ㄸ",
+			input: []rune{'ㅃ', 'ㅉ', 'ㅏ', 'ㄸ'},
+			want:  "ㅃ짜ㄸ",
+		},
+		{
+			name:  "ㅒㅗㅒ",
+			input: []rune{'ㅒ', 'ㅗ', 'ㅒ'},
+			want:  "ㅒㅗㅒ",
+		},
+		{
+			name:  "쀍ㅅ",
+			input: []rune{'ㅃ', 'ㅜ', 'ㅔ', 'ㄹ', 'ㄱ', 'ㅅ'},
+			want:  "쀍ㅅ",
+		},
+		{
+			name:  "쀌가",
+			input: []rune{'ㅃ', 'ㅜ', 'ㅔ', 'ㄹ', 'ㄱ', 'ㅏ'},
+			want:  "쀌가",
+		},
+		{
+			name:  "쀌궬궭ㅂ",
+			input: []rune{'ㅃ', 'ㅞ', 'ㄹ', 'ㄱ', 'ㅞ', 'ㄹ', 'ㄱ', 'ㅞ', 'ㄹ', 'ㄱ', 'ㅂ'},
+			want:  "쀌궬궭ㅂ",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := Assemble(tt.input)
+			if !assert.Equal(t, tt.want, result) {
+				t.Errorf("Assemble() = %v, want %v", result, tt.want)
+			}
+		})
+	}
+}
+
+func TestIsComplete(t *testing.T) {
+	tests := []struct {
+		name   string
+		input  rune
+		result bool
+	}{
+		{
+			name:   "한",
+			input:  '한',
+			result: true,
+		},
+		{
+			name:   "ㄱ",
+			input:  'ㄱ',
+			result: false,
+		},
+		{
+			name:   "ㅙ",
+			input:  'ㅙ',
+			result: false,
+		},
+		{
+			name:   "a",
+			input:  'a',
+			result: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := IsComplete(tt.input)
+			if !assert.Equal(t, tt.result, result) {
+				t.Errorf("IsComplete() = %v, want %v", result, tt.result)
+			}
+		})
+	}
+}
+
+func TestIsConsonant(t *testing.T) {
+	tests := []struct {
+		name   string
+		input  rune
+		result bool
+	}{
+		{
+			name:   "한",
+			input:  '한',
+			result: false,
+		},
+		{
+			name:   "ㄱ",
+			input:  'ㄱ',
+			result: true,
+		},
+		{
+			name:   "ㅙ",
+			input:  'ㅙ',
+			result: false,
+		},
+		{
+			name:   "a",
+			input:  'a',
+			result: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := IsConsonant(tt.input)
+			if !assert.Equal(t, tt.result, result) {
+				t.Errorf("IsConsonant() = %v, want %v", result, tt.result)
+			}
+		})
+	}
+}
+
+func TestIsVowel(t *testing.T) {
+	tests := []struct {
+		name   string
+		input  rune
+		result bool
+	}{
+		{
+			name:   "한",
+			input:  '한',
+			result: false,
+		},
+		{
+			name:   "ㄱ",
+			input:  'ㄱ',
+			result: false,
+		},
+		{
+			name:   "ㅙ",
+			input:  'ㅙ',
+			result: true,
+		},
+		{
+			name:   "a",
+			input:  'a',
+			result: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := IsVowel(tt.input)
+			if !assert.Equal(t, tt.result, result) {
+				t.Errorf("IsVowel() = %v, want %v", result, tt.result)
+			}
+		})
+	}
+}
+
+func TestIsCho(t *testing.T) {
+	tests := []struct {
+		name   string
+		input  rune
+		result bool
+	}{
+		{
+			name:   "ㄱ",
+			input:  'ㄱ',
+			result: true,
+		},
+		{
+			name:   "ㄸ",
+			input:  'ㄸ',
+			result: true,
+		},
+		{
+			name:   "ㄳ",
+			input:  'ㄳ',
+			result: false,
+		},
+		{
+			name:   "ㅏ",
+			input:  'ㅏ',
+			result: false,
+		},
+		{
+			name:   "a",
+			input:  'a',
+			result: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := IsCho(tt.input)
+			if !assert.Equal(t, tt.result, result) {
+				t.Errorf("IsCho() = %v, want %v", result, tt.result)
+			}
+		})
+	}
+}
+
+func TestIsJong(t *testing.T) {
+	tests := []struct {
+		name   string
+		input  rune
+		result bool
+	}{
+		{
+			name:   "ㄱ",
+			input:  'ㄱ',
+			result: true,
+		},
+		{
+			name:   "ㄸ",
+			input:  'ㄸ',
+			result: false,
+		},
+		{
+			name:   "ㄳ",
+			input:  'ㄳ',
+			result: true,
+		},
+		{
+			name:   "ㅏ",
+			input:  'ㅏ',
+			result: false,
+		},
+		{
+			name:   "a",
+			input:  'a',
+			result: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := IsJong(tt.input)
+			if !assert.Equal(t, tt.result, result) {
+				t.Errorf("IsJong() = %v, want %v", result, tt.result)
+			}
+		})
+	}
+}
+
+func TestIsCompleteAll(t *testing.T) {
+	tests := []struct {
+		name   string
+		input  string
+		result bool
+	}{
+		{
+			name:   "한글",
+			input:  "한글",
+			result: true,
+		},
+		{
+			name:   "한글ㄱ",
+			input:  "한글ㄱ",
+			result: false,
+		},
+		{
+			name:   "한글a",
+			input:  "한글a",
+			result: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := IsCompleteAll(tt.input)
+			if !assert.Equal(t, tt.result, result) {
+				t.Errorf("IsCompleteAll() = %v, want %v", result, tt.result)
+			}
+		})
+	}
+}
+
+func TestIsConsonantAll(t *testing.T) {
+	tests := []struct {
+		name   string
+		input  string
+		result bool
+	}{
+		{
+			name:   "한ㄱ",
+			input:  "한ㄱ",
+			result: false,
+		},
+		{
+			name:   "ㅎㄱ",
+			input:  "ㅎㄱ",
+			result: true,
+		},
+		{
+			name:   "ㅁa",
+			input:  "ㅁa",
+			result: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := IsConsonantAll(tt.input)
+			if !assert.Equal(t, tt.result, result) {
+				t.Errorf("IsConsonantAll() = %v, want %v", result, tt.result)
+			}
+		})
+	}
+}
+
+func TestIsVowelAll(t *testing.T) {
+	tests := []struct {
+		name   string
+		input  string
+		result bool
+	}{
+		{
+			name:   "한글",
+			input:  "한글",
+			result: false,
+		},
+		{
+			name:   "ㅗㄱ",
+			input:  "ㅗㄱ",
+			result: false,
+		},
+		{
+			name:   "ㅙㅜ",
+			input:  "ㅙㅜ",
+			result: true,
+		},
+		{
+			name:   "ㅙa",
+			input:  "ㅙa",
+			result: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := IsVowelAll(tt.input)
+			if !assert.Equal(t, tt.result, result) {
+				t.Errorf("IsVowelAll() = %v, want %v", result, tt.result)
+			}
+		})
+	}
+}
+
+func TestIsChoAll(t *testing.T) {
+	tests := []struct {
+		name   string
+		input  string
+		result bool
+	}{
+		{
+			name:   "ㄱㄴㄷ",
+			input:  "ㄱㄴㄷ",
+			result: true,
+		},
+		{
+			name:   "ㄸㄲㅆ",
+			input:  "ㄸㄲㅆ",
+			result: true,
+		},
+		{
+			name:   "ㄱㄴㄳ",
+			input:  "ㄱㄴㄳ",
+			result: false,
+		},
+		{
+			name:   "ㄱㄴㅏ",
+			input:  "ㄱㄴㅏ",
+			result: false,
+		},
+		{
+			name:   "ㄱㄴa",
+			input:  "ㄱㄴa",
+			result: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := IsChoAll(tt.input)
+			if !assert.Equal(t, tt.result, result) {
+				t.Errorf("IsChoAll() = %v, want %v", result, tt.result)
+			}
+		})
+	}
+}
+
+func TestIsJongAll(t *testing.T) {
+	tests := []struct {
+		name   string
+		input  string
+		result bool
+	}{
+		{
+			name:   "ㄱㄴ",
+			input:  "ㄱㄴ",
+			result: true,
+		},
+		{
+			name:   "ㄱㄸ",
+			input:  "ㄱㄸ",
+			result: false,
+		},
+		{
+			name:   "ㄳㅄ",
+			input:  "ㄳㅄ",
+			result: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := IsJongAll(tt.input)
+			if !assert.Equal(t, tt.result, result) {
+				t.Errorf("IsJongAll() = %v, want %v", result, tt.result)
+			}
+		})
+	}
+}
+
+func TestEndsWithConsonant(t *testing.T) {
+	tests := []struct {
+		name   string
+		input  string
+		result bool
+	}{
+		{
+			name:   "ㄱ",
+			result: true,
+			input:  "ㄱ",
+		},
+		{
+			name:   "가",
+			result: false,
+			input:  "가",
+		},
+		{
+			name:   "ㅏ",
+			result: false,
+			input:  "ㅏ",
+		},
+		{
+			name:   "각",
+			result: true,
+			input:  "각",
+		},
+		{
+			name:   "가각",
+			result: true,
+			input:  "가각",
+		},
+		{
+			name:   "abc각",
+			result: true,
+			input:  "abc각",
+		},
+		{
+			name:   "abc",
+			result: false,
+			input:  "abc",
+		},
+		{
+			name:   "各",
+			result: false,
+			input:  "各",
+		},
+		{
+			name:   "!@#",
+			result: false,
+			input:  "!@#",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := EndsWithConsonant(tt.input)
+			if !assert.Equal(t, tt.result, result) {
+				t.Errorf("EndsWithConsonant() = %v, want %v", result, tt.result)
+			}
+		})
+	}
+}
+
+func TestEndsWith(t *testing.T) {
+	tests := []struct {
+		name   string
+		input  string
+		target rune
+		result bool
+	}{
+		{
+			name:   "\"ㄱ\" 'ㄱ'",
+			input:  "ㄱ",
+			target: 'ㄱ',
+			result: true,
+		},
+		{
+			name:   "\"가\" 'ㄱ'",
+			input:  "가",
+			target: 'ㄱ',
+			result: false,
+		},
+		{
+			name:   "\"가\" 'ㅏ'",
+			input:  "가",
+			target: 'ㅏ',
+			result: true,
+		},
+		{
+			name:   "\"각\" 'ㄱ'",
+			input:  "각",
+			target: 'ㄱ',
+			result: true,
+		},
+		{
+			name:   "\"abc\" 'c'",
+			input:  "abc",
+			target: 'c',
+			result: true,
+		},
+		{
+			name:   "\"abc\" 'b'",
+			input:  "abc",
+			target: 'b',
+			result: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := EndsWith(tt.input, tt.target)
+			if !assert.Equal(t, tt.result, result) {
+				t.Errorf("EndsWith() = %v, want %v", result, tt.result)
+			}
+		})
+	}
+}
